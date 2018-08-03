c40e1c5a3f24f9e00cc80c975abcc692018f0c2b
Adontifrice <41913341+Adontifrice@users.noreply.github.com>
2018-08-03 09:29:21 -0400

    Add files via upload

---
+++ Sismiques/src/main/java/Sisous/Sismiques/App.java
@@ -0,0 +1,13 @@
+package Sisous.Sismiques;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Doing Moyennemnt Hard Stuff" );
+    }
+}
